ff3effd777530fa2ccb4c62571bd3f978531d1b7
Flavio Grossi <97956776+Fgrossi-42@users.noreply.github.com>
2022-02-15 19:18:16 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
-//nel nostro é presente un BUFFER_SIZE ma un semplice BUFFER_SIZE *= 0 + 1
-//sembra bastare per rendere il codice identico
+#include <fcntl.h>
+//nel nostro é presente un BUFFER_SIZE ma un semplice BUFFER_SIZE * 0 + 1
+//sembra bastare per rendere il codice estremamente semplice
 char	*get_next_line(int fd)
 {
 	int	rd;
@@ -11,7 +11,7 @@ char	*get_next_line(int fd)
 	char	*buffer = malloc(9999);
 
 	i = 0;
-	while ((rd = read(fd, &c, 1) > 0))
+	while ((rd = read(fd, &c, BUFFER_SIZE * 0 + 1)) > 0)
 	{
 		buffer[i] = c;
 		i++;
@@ -23,7 +23,6 @@ char	*get_next_line(int fd)
 		free(buffer);
 		return (NULL);
 	}
-	i++;
 	buffer[i] = '\0';
 	return (buffer);
 }
