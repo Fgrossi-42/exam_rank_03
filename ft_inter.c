6852b320e90bf34aff3d8962e2f07d944e1da15a
Flavio Grossi <97956776+Fgrossi-42@users.noreply.github.com>
2022-05-08 19:14:53 +0200

    Update ft_inter.c

--- ft_inter.c
+++ ft_inter.c
@@ -5,37 +5,36 @@ ex:tumid fleck, null
 ex:bellissima bellissima, belisma
 ex:armadio ardella, ard
 */
-int check(char *argv, char c, int i)
-{
-	//controllo a ritroso di eventuali doppie di 'c' presenti in argv, a partire da index 'i'
-	while (i-- >= 1)
-	{
-		if (c == argv[i])
-			return (1);
-	}
-	return (0);
-}
+#include <unistd.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	int	i;
-	int	j;
-	
+	int	used[255];
+	int	i, j;
+
 	if (argc == 3)
 	{
-		i = -1;
-		while (argv[1][++i] != '\0')
+		i = 0;
+		while (i < 255)
+			used[i++] = 0;
+		i = 2;
+		while (i > 0)
 		{
-			j = -1;
-			while (argv[2][++j] != '\0')
+			j = 0;
+			while (argv[i][j])
 			{
-				if ((argv[1][i] == argv[2][j]) && !(check(argv[1], argv[1][i], i)))
+				if (i == 2 && !used[(unsigned char)argv[i][j]])
+					used[(unsigned char)argv[i][j]] = 1;
+				else if (i == 1 && used[(unsigned char)argv[i][j]] == 1)
 				{
-					write(1, &argv[1][i], 1);
-					break;
+					write(1, &argv[i][j], 1);
+					used[(unsigned char)argv[i][j]] = 2;
 				}
+				j++;
 			}
+			i--;
 		}
 	}
 	write(1, "\n", 1);
+	return (0);
 }
